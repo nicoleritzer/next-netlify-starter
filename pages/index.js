d2c2ede96519cc35dd31a5f924d98c59d49410a5
nicole <42187709+nicoleritzer@users.noreply.github.com>
2023-10-01 10:09:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,122 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<style>
+    * {
+        font-family: 'Source Code Pro', monospace;
+      }
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      .page {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
 
-      <Footer />
+        display: flex;
+        flex-direction: column;
+        font-size: 0.9em;
+      }
+
+      header {
+        display: flex;
+        align-items: center;
+        padding: 10px 20px;
+      }
+
+      header h1 {
+        margin: 0;
+        padding-right: 10px;
+      }
+
+      header a {
+        color: #08f;
+        text-decoration: none;
+        padding: 0px 10px;
+      }
+
+      header a:hover {
+        text-decoration: underline;
+      }
+
+      .converter {
+        display: flex;
+        flex: 1;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .split {
+        display: flex;
+        align-self: stretch;
+        flex-direction: column;
+        flex: 1;
+        box-shadow: 1px 0 0 #eee;
+      }
+
+      .pane {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        align-self: stretch;
+        padding: 20px;
+        font-size: 1em;
+        border: none;
+        margin: 0px;
+        outline: none;
+        overflow: auto;
+      }
+
+      .pane label {
+        display: block;
+        text-transform: uppercase;
+        color: #555;
+        margin-bottom: 5px;
+      }
+
+      .pane-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #eee;
+        padding: 10px 20px;
+      }
+
+      #source {
+        flex: 1;
+        display: flex;
+        align-self: stretch;
+        border: none;
+        background-color: transparent;
+        outline: none;
+        padding: 0px;
+        font-size: inherit;
+      }
+
+      #output {
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+      <div className="container">
+    <div class='page'>
+      <div class='converter'>
+        <div class='split'>
+          <div class='pane-header'>
+            
+          </div>
+          <div class='pane pane-source'>
+            <textarea id='source' placeholder='<div>HTML to transform</div>'></textarea>
+          </div>
+        </div>
+        <div class='split'>
+          <div class='pane-header'>
+          </div>
+          <div class='pane pane-output'>
+            <pre id='output'></pre>
+          </div>
+        </div>
+      </div> 
     </div>
   )
 }
